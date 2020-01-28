5b2d64228dc5ad3aa5a700c23161a2f8b482e528
Kiara Evers <k.s.evers@hotmail.comn>
2020-01-28 19:53:50 +0100

    final version

--- code/algorithms/random_points_ascending_hillclimber.py
+++ code/algorithms/random_points_ascending_hillclimber.py
@@ -27,59 +27,49 @@ def random_points_ascending_hillclimber_algoritme(number_of_houses, map_number):
     return total_value, all_houses_dic, waters
 
 def random_points_ascending_hillclimber(house, all_houses, waters, total_value_map):
-    ''' Pakt random een gekozen aantal punten per huis en kijkt of op die locaties totale waarde van de map hoger wordt '''
-   
-    # initialiseer nieuwe totale waarde map
+    """pakt random 100 punten en kijkt of de totale waarde hoger wordt"""
     total_value_map_NEW = total_value_map
 
-    # check in welke range het huis geplaats kan worden, niet kijkend naar water of andere huizen
+    # check in welke range het huis geplaats kan worden, niet kijkend naar water of andere 
     rangex = MAXIMUM_WIDTH - house.width
     rangey = MAXIMUM_HEIGHT - house.length
-    # range die aangeeft hoeveel random punten algoritme langs gaat
+
     for x in range(100):
 
-        if house.placed == True:
-        # maak random x en y coördinaat
-            randomizex = rangex * random()
-            randomizey = rangey * random()
+        randomizex = rangex * random()
+        randomizey = rangey * random()
 
-            # sla oude coördinaat op
-            tempx = house.bottom_left[0]
-            tempy = house.bottom_left[1]
+        tempx = house.bottom_left[0]
+        tempy = house.bottom_left[1]
+        # save cordinates house.bottom_left
+        bottom_left = (randomizex,randomizey)
+        # change cordinates of bl to new location
+        house.location(bottom_left)
+        # if you can place house on new location
+        if place_house(house, all_houses, waters) == True:
+            # bereken nieuw waarde map, waarin huis is verplaatst
+            total_value_map_temp = 0
+            for item in all_houses.values():
+                for house in item:
+                    house.extra_meters()
+                    total_value_map_temp += house.totalprice()
 
-            # verander locatie van huis naar nieuwe locatie
-            bottom_left = (randomizex,randomizey)
-            house.location(bottom_left)
-
-            # als het huis op de nieuwe locatie geplaatst kan worden
-            if place_house(house, all_houses, waters) == True:
-                total_value_map_temp = 0
-                # bereken nieuw waarde map, waarin huis is verplaatst
-                for item in all_houses.values():
-                    for house in item:
-                        house.extra_meters()
-                        total_value_map_temp += house.totalprice()
-
-                # als waarde met nieuwe locatie hoger is, verander deze
-                if total_value_map_NEW < total_value_map_temp:
-                    total_value_map_NEW = total_value_map_temp
-                    
-                # als waarde niet hoger is, verander naar oude locatie en bereken weer totale waarde map
-                else:
-                    bottom_left = (tempx,tempy)
-                    house.location(bottom_left)
-                    if place_house(house, all_houses, waters) == True:
-                        for item in all_houses.values():
-                            for houses in item:
-                                houses.extra_meters()
-                                houses.totalprice()
-
-            # als huis niet geplaats kan worden, verander naar oude locatie en bereken weer totale waarde map
+            # als waarde met nieuwe locatie hoger is, verander deze
+            if total_value_map_NEW < total_value_map_temp:
+                total_value_map_NEW = total_value_map_temp
+            # als waarde niet hoger is verander naar oude locatie
             else:
                 bottom_left = (tempx,tempy)
                 house.location(bottom_left)
-                print(place_house(house, all_houses, waters), house.name)
                 if place_house(house, all_houses, waters) == True:
+                    for item in all_houses.values():
+                        for houses in item:
+                            houses.extra_meters()
+                            houses.totalprice()
+        else:
+            bottom_left = (tempx,tempy)
+            house.location(bottom_left)
+            if place_house(house, all_houses, waters) == True:
                     for item in all_houses.values():
                         for houses in item:
                             houses.extra_meters()
--- resultaten/README.md
+++ resultaten/README.md
@@ -2,22 +2,22 @@
 Onze resultaten worden opgeslagen in een csv file (document dat gescheiden wordt door middel van komma's).
 Er zijn meerdere csv files te vinden, in progress_run worden de resultanten gevonden van de huidige run. Wanneer het programma beeindigt wordt word dit document automatisch geleegd. In solutions.csv worden alle behaalde resultaten opgeslagen van elke run die gedaan is, deze file wordt dus nooit geleegd en wordt bij elke run steeds groter.
 
-De resulaten in progress_run worden als volgt opgeslagen:
-argument 1: aantal gekozen huizen
-argument 2: gekozen map nummer
-argument 3: totale waarde die behaalt is met het algoritme
-argument 4: het algoritme dat gebruikt is om het resultaat te behalen
+De resulaten in progress_run worden als volgt opgeslagen:<br>
+argument 1: aantal gekozen huizen<br>
+argument 2: gekozen map nummer<br>
+argument 3: totale waarde die behaalt is met het algoritme<br>
+argument 4: het algoritme dat gebruikt is om het resultaat te behalen<br>
 
-De resultaten in solutions worden als volgt opgeslagen:
-Het begint met een begin regel waarbij;
-argument 1: aantal gekozen huizen
-argument 2: gekozen map nummer
-argument 3: totale waarde die behaalt is met het algoritme
-argument 4: het algoritme dat gebruikt is om het resultaat te behalen
+De resultaten in solutions worden als volgt opgeslagen:<br>
+Het begint met een begin regel waarbij;<br>
+argument 1: aantal gekozen huizen<br>
+argument 2: gekozen map nummer<br>
+argument 3: totale waarde die behaalt is met het algoritme<br>
+argument 4: het algoritme dat gebruikt is om het resultaat te behalen<br>
 
-vervolgens komen er het gekozen aantal huizen aan regels onder, 
-Deze huizen worden per regel beschreven.
-argument 1: naam van de woning
-argument 2: grootte van de woning
-argument 3: x en y coordinaat van de linker onderhoek
-argument 4: x en y coordinaat van de rechter bovenhoek
+vervolgens komen er het gekozen aantal huizen aan regels onder, <br>
+Deze huizen worden per regel beschreven.<br>
+argument 1: naam van de woning<br>
+argument 2: grootte van de woning<br>
+argument 3: x en y coordinaat van de linker onderhoek<br>
+argument 4: x en y coordinaat van de rechter bovenhoek<br>
